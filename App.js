3b3200b8a3ef9de537ce770802d2f44c30eedc64
Khun Kyaw Kham Hti <khunkyawkhamhti@Khuns-Air.local>
2023-02-01 11:46:08 +0630

    first commit
    some description

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+} from "react-native-reanimated";
+import { prepareUIRegistry } from "react-native-reanimated/lib/reanimated2/frameCallback/FrameCallbackRegistryUI";
+
+const SIZE = 100.0;
+
+const handleRotate = (progress) => {
+  "worklet";
+  return `${progress.value * 2 * Math.PI}rad`
+}
 
 export default function App() {
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(2);
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      borderRadius: progress.value * SIZE / 2,
+      transform: [{ scale: scale.value }, { rotate: handleRotate(progress) }],
+    };
+  }, []);
+
+  useEffect(() => {
+    progress.value = withRepeat(withSpring(0.5), 5, true);
+    scale.value = withRepeat(withSpring(1), 5, true);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Animated.View style={[{ width: SIZE, height: SIZE, backgroundColor: "blue" }, reanimatedStyle]} />
+      <Animated.View style={[{ width: SIZE, height: SIZE, backgroundColor: "#AAE3E2" }, reanimatedStyle]} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +45,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
